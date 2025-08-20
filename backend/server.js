7cedf942f9de0888a3d719b86c078ac4b1c6ab54
suhani001-sudo <suhanipcte@gmail.com>
2025-08-20 22:50:13 +0530

    Initial commit with frontend and backend

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log(' MongoDB Connected'))
+.catch(err => console.log('MongoDB connection error:', err));
+
+app.get('/', (req, res) => {
+  res.send('Food Allergy API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
